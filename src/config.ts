3c546be5942d427253091e666e61f019c60ddd62
Kus Cámara <kcmr@users.noreply.github.com>
2025-05-21 23:30:16 +0200

    chore: test commit

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
-import * as path from 'path'
 import * as vscode from 'vscode'
+import * as path from 'path'
 
 export interface YamlLintFixConfig {
   yamllintPath: string
